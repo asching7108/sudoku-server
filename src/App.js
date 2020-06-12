44a4b3be4beec41ce3fc5b9fbb0ebca931859a54
asching7108 <asching7108@gmail.com>
2020-06-11 23:27:44 -0400

    Fix default endpoint response issue

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ app.use('/api/puzzles', PuzzlesRouter);
 app.use('/api/records', RecordsRouter);
 
 app.get('/', (req, res) => {
-	res.send('Hello, world!');
+	res.json('Hello, world!');
 });
 
 /* error handler */
