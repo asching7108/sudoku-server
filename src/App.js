fcb1120a988b73e07ae2ea710d637f0cb9a7e292
asching7108 <asching7108@gmail.com>
2021-07-10 01:13:31 -0400

    temp updates for debugging on heroku

--- src/App.js
+++ src/App.js
@@ -31,12 +31,8 @@ app.get('/', (req, res) => {
 /* error handler */
 app.use((error, req, res, next) => {
 	let response;
-	if (NODE_ENV === 'production') {
-		response = { error: { message: 'server error' }};
-	} else {
-		console.error(error);
-		response = { message: error.message, error };
-	}
+	console.error(error);
+	response = { message: error.message, error };
 	res.status(500).json(response);
 });
 
