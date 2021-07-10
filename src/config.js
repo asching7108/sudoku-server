fd764bc943a14fa8fd5d003885b70b2a8d22eb6d
asching7108 <asching7108@gmail.com>
2021-07-09 23:34:00 -0400

    Updated frontend app url in config file

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
 	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://esther_lin@localhost/sudoku-test',
 	JWT_SECRET: process.env.JWT_SECRET || 'jwt-secret-esther-lin-2020',
 	JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://sudoku-client.now.sh'
+	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://sudoku-web.vercel.app'
 };
