b5aca553af87149ec4b97c755301a31cd8ae29a0
asching7108 <asching7108@gmail.com>
2021-07-10 01:57:28 -0400

    Removed temp updatesfor debugging on heroku

--- src/App.js
+++ src/App.js
@@ -31,8 +31,12 @@ app.get('/', (req, res) => {
 /* error handler */
 app.use((error, req, res, next) => {
 	let response;
-	console.error(error);
-	response = { message: error.message, error };
+	if (NODE_ENV === 'production') {
+		response = { error: { message: 'server error' }};
+	} else {
+		console.error(error);
+		response = { message: error.message, error };
+	}
 	res.status(500).json(response);
 });
 
